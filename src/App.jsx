b3e1992a6ff8f34228f926df64ff4b04616be2f3
Carlo Costa <carlo.costa92@gmail.com>
2025-04-16 13:20:39 +0200

    feat: set routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+//importo il layout dove dentro c'è la logoca della navbar
+import DefaultLayout from './components/DefaultLayout'
+//importo le pagine
+import HomePage from './pages/Home'
+import AboutUs from './pages/About'
+import PostList from './pages/PostList'
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="about" element={<AboutUs />} />
+          <Route path="posts" element={<PostList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
