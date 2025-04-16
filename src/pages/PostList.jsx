3bde6ecde624b4f68da993c7ae60179856be92ac
Carlo Costa <carlo.costa92@gmail.com>
2025-04-16 13:33:54 +0200

    feat: called posts from api

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,9 +1,30 @@
+import axios from 'axios';
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 function PostList() {
+
+    //variabile per salvare i post
+    const [posts, setPosts] = useState([]);
+
+    const endpoint = "https://jsonplaceholder.typicode.com/posts";
+    //chiamata axios
+    useEffect(() => {
+        axios.get(endpoint)
+            .then(response => {
+                setPosts(response.data);
+            })
+    }, []);
+
+
     return (
         <div>
             <h1>Lista dei Post</h1>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
         </div>
     );
 }
